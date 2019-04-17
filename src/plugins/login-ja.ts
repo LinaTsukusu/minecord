3f9ad24eb26aa016abf6e1964e70e3d075ecf632
neverclear86 <neverclear86sab@gmail.com>
2019-04-17 17:23:30 +0900

    TS化

--- src/plugins/login-ja.ts
+++ src/plugins/login-ja.ts
@@ -1,14 +1,15 @@
 import Replacers from '../Replacers'
+import {EventEmitter} from 'events'
 
 const replacers = (new Replacers)
   .add(/^(.*)\sjoined\sthe\sgame$/, (message, player) => `${player} がログインしたみたい。`)
   .add(/^(.*)\sleft\sthe\sgame$/, (message, player) => `${player} がログアウトしたみたい。`)
 
-export default Plugin => new Plugin({
-  async minecraft ({causedAt, level, message, sendToDiscord}) {
+export default (plugin: EventEmitter) => {
+  plugin.on('minecraft', async ({causedAt, level, message, sendToDiscord}: MinecraftArgs) => {
     if (causedAt !== 'Server thread' || level !== 'INFO') return
 
     const newMessage = replacers.replace(message)
     if (newMessage !== false) await sendToDiscord(newMessage)
-  }
-})
+  })
+}
