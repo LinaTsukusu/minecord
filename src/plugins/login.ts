5a6b35c8793cf18f9a7099d653660637ffe4ada9
neverclear86 <neverclear86sab@gmail.com>
2019-04-17 17:21:24 +0900

    TS化

--- src/plugins/login.ts
+++ src/plugins/login.ts
@@ -1,13 +1,15 @@
+import {EventEmitter} from 'events'
+
 const regexpArray = [
   /^(.*)\sjoined\sthe\sgame$/,
   /^(.*)\sleft\sthe\sgame$/,
 ]
 
-export default Plugin => new Plugin({
-  async minecraft ({causedAt, level, message, sendToDiscord}) {
+export default (plugin: EventEmitter) => {
+  plugin.on('minecraft', async ({causedAt, level, message, sendToDiscord}: MinecraftArgs) => {
     if (causedAt !== 'Server thread' || level !== 'INFO') return
 
     if (regexpArray.some(regexp => regexp.test(message)))
       await sendToDiscord(message)
-  }
-})
+  })
+}
